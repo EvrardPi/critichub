212e3a0892e5e0a38b97c287cbdbad68deaf970c
Pierre EVRARD <evrardpierrefm@gmail.com>
2023-06-25 14:51:10 +0200

    fix: error conflicts login and register

--- app/Middlewares/CheckAuth.php
+++ app/Middlewares/CheckAuth.php
@@ -5,9 +5,11 @@ namespace App\Middlewares;
 class CheckAuth
 {
 
-    static public function isLoggedIn() 
+    static public function isLoggedIn()
     {
-        return $_SESSION['isAuth'] ? true : false;
+        if (isset($_SESSION['isAuth'])) {
+            return $_SESSION['isAuth'] ? true : false;
+        }
+        return false; // or handle the case when 'isAuth' is not set
     }
-    
 }
