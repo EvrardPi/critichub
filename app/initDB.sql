1f4b416690b3157f349798a150e24e0fe210d9fc
Pierre EVRARD <evrardpierrefm@gmail.com>
2023-07-20 23:04:43 +0200

    fix: struct update in initdb

--- app/initDB.sql
+++ app/initDB.sql
@@ -1,106 +1,85 @@
--- DROP TABLE IF EXISTS paya4_elementard;
--- DROP TABLE IF EXISTS paya4_memento;
--- DROP TABLE IF EXISTS paya4_gestionfront;
--- DROP TABLE IF EXISTS paya4_category;
--- DROP TABLE IF EXISTS paya4_user;
--- CREATE TABLE paya4_user
--- (
---     id serial primary key,
---     email varchar(320) not null unique,
---     lastname varchar(120) not null,
---     firstname varchar(60) not null,
---     password varchar(255) not null,
---     role integer default 1 not null,
---     birth_date date not null,
---     creation_date timestamp with time zone default CURRENT_TIMESTAMP,
---     update_date timestamp with time zone default CURRENT_TIMESTAMP,
---     confirm_key varchar(255) unique,
---     confirm integer,
---     forgot_token varchar(32),
---     expiration_time integer
--- );
--- ALTER TABLE paya4_user OWNER TO postgres;
--- CREATE TABLE paya4_category
--- (
---     id serial primary key,
---     name varchar(255),
---     picture varchar(255)
--- );
--- ALTER TABLE paya4_category OWNER TO postgres;
--- CREATE TABLE paya4_gestionfront
--- (
---     h1 varchar(255),
---     h2 varchar(255),
---     h3 varchar(255),
---     h4 varchar(255),
---     h5 varchar(255),
---     h6 varchar(255),
---     text varchar(255),
---     background varchar(255),
---     link varchar(255),
---     strong varchar(255),
---     span varchar(255)
--- );
--- ALTER TABLE paya4_gestionfront OWNER TO postgres;
--- CREATE TABLE paya4_memento
--- (
---     id_memento serial primary key,
---     content text
--- );
--- ALTER TABLE paya4_memento OWNER TO postgres;
--- CREATE TABLE "paya4_elementard" 
--- (
---     id SERIAL PRIMARY KEY,
---     background_color VARCHAR(25) NOT NULL,
---     categories VARCHAR(255),
---     categories_color VARCHAR(255),
---     critique TEXT NOT NULL,
---     critique_background_color VARCHAR(255) NOT NULL,
---     date_sortie INT NOT NULL,
---     director_name VARCHAR(255) NOT NULL,
---     font VARCHAR(255) NOT NULL,
---     font_color VARCHAR(25) NOT NULL,
---     font_textarea_color VARCHAR(255) NOT NULL,
---     image_url VARCHAR(255) NOT NULL,
---     movie_name VARCHAR(255) NOT NULL,
---     movie_time INT NOT NULL,
---     note INT NOT NULL,
---     slogan_movie VARCHAR(255) NOT NULL,
---     template VARCHAR(255) NOT NULL,
---     nb_vue INT NOT NULL,
---     create_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
---     id_user INT,
---     FOREIGN KEY (id_user) REFERENCES paya4_user (id) ON DELETE SET NULL
--- );
--- ALTER TABLE paya4_elementard OWNER TO postgres;
+DROP TABLE IF EXISTS paya4_comment;
+DROP TABLE IF EXISTS paya4_elementard;
+DROP TABLE IF EXISTS paya4_user;
+DROP TABLE IF EXISTS paya4_category;
+DROP TABLE IF EXISTS paya4_gestionfront;
+DROP TABLE IF EXISTS paya4_memento;
 
-INSERT INTO
-    public.paya4_user (
-        email,
-        lastname,
-        firstname,
-        password,
-        role,
-        birth_date,
-        creation_date,
-        update_date,
-        confirm_key,
-        confirm,
-        forgot_token,
-        expiration_time
-    )
-VALUES
-    (
-        'adam.brooown@gmail.com',
-        'Broooown',
-        'Adam',
-        '$2y$10$jYGyaE8S287U54NR/62Ore10A3yBfDfCjFYDbzZfRmMKNNSFPR8xm',
-        3,
-        '1980-01-01',
-        '2021-01-18 14:48:30.102016 +00:00',
-        '2023-07-19 14:48:30.102016 +00:00',
-        '2c153ffe265fc76f023d2e4838c0438305abe498967299d7a7d94506db8b0d7e',
-        1,
-        null,
-        null
-    );
+CREATE TABLE paya4_category
+(
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    picture varchar(255) NOT NULL
+);
+
+CREATE TABLE paya4_user
+(
+    id serial PRIMARY KEY,
+    email varchar(320) NOT NULL UNIQUE,
+    lastname varchar(120) NOT NULL,
+    firstname varchar(60) NOT NULL,
+    password varchar(255) NOT NULL,
+    role integer NOT NULL DEFAULT 1,
+    birth_date date NOT NULL,
+    creation_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    update_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    confirm_key varchar(255) UNIQUE,
+    confirm integer NOT NULL,
+    forgot_token varchar(32),
+    expiration_time integer
+);
+
+CREATE TABLE paya4_elementard
+(
+    id serial PRIMARY KEY,
+    background_color varchar(25) NOT NULL,
+    categories varchar(255),
+    categories_color varchar(255),
+    critique text NOT NULL,
+    critique_background_color varchar(255) NOT NULL,
+    date_sortie integer NOT NULL,
+    director_name varchar(255) NOT NULL,
+    font varchar(255) NOT NULL,
+    font_color varchar(25) NOT NULL,
+    font_textarea_color varchar(255) NOT NULL,
+    image_url varchar(255) NOT NULL,
+    movie_name varchar(255) NOT NULL,
+    movie_time integer NOT NULL,
+    note integer NOT NULL,
+    slogan_movie varchar(255) NOT NULL,
+    template varchar(255) NOT NULL,
+    nb_vue integer NOT NULL,
+    create_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    id_user integer REFERENCES paya4_user ON DELETE SET NULL
+);
+
+CREATE TABLE paya4_comment
+(
+    id serial PRIMARY KEY,
+    content text NOT NULL,
+    status integer NOT NULL,
+    id_review integer NOT NULL REFERENCES paya4_elementard ON DELETE CASCADE,
+    id_user integer REFERENCES paya4_user ON DELETE SET NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE paya4_gestionfront
+(
+    h1 varchar(255),
+    h2 varchar(255),
+    h3 varchar(255),
+    h4 varchar(255),
+    h5 varchar(255),
+    h6 varchar(255),
+    text varchar(255),
+    background varchar(255),
+    link varchar(255),
+    strong varchar(255),
+    span varchar(255)
+);
+
+CREATE TABLE paya4_memento
+(
+    id_memento serial PRIMARY KEY,
+    content text NOT NULL
+);
