0d63bb142c95d792b8977a31a925a1cafc9817ed
KHERRAZ Mohammed Achraf <achraf.sena@gmail.com>
2023-07-20 10:38:19 +0200

    feat: front css comment

--- app/Views/MediaPage/media.view.php
+++ app/Views/MediaPage/media.view.php
@@ -2,12 +2,33 @@
 
 </div>
 
+
 <?php $this->partial("form", $comments); ?>
 
-<?php foreach (array_reverse($commentData) as $comment): ?>
-    <div><?php echo $comment['firstName'] . ' ' . $comment['lastName']; ?></div>
-    <div><?php echo $comment['content']; ?></div>
-    <div><?php echo substr($comment['createdAt'], 0, 16); ?></div>
-    <hr>
-<?php endforeach; ?>
+<div class="commentaire-section">
+    <h2>Commentaires</h2>
+    <label for="content">Écrit ton commentaire:</label>
+    <textarea class="add_comment" rows="5" name="content" placeholder="Enter votre commentaire ?"></textarea>
+    <button class="btn-comment">Envoyer</button>
+    <div class="scroll-comment">
+    <?php foreach (array_reverse($commentData) as $comment): ?>
+        <div class="commentaire-content">
+            <div class="auth">
+                <h3>
+                    <?php echo $comment['firstName'] . ' ' . $comment['lastName']; ?>
+                </h3>
+            </div>
+            <div class="content">
+                <p>
+                    <?php echo $comment['content']; ?>
+                </p>
+            </div>
+            <div class="date-comment">
+                <?php echo substr($comment['createdAt'], 0, 16); ?>
+            </div>
+        </div>
+    <?php endforeach; ?>
+    </div>
 
+
+</div>
